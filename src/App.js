8cc59cb7176d56869c4d856d3a54436aa2a1d2b0
amandhunna <amandhunna.dev@gmail.com>
2023-03-14 16:29:03 +0530

    feat: load data from the shape file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, {useState, useEffect} from 'react'
 
+ /**
+  * STEP 2.1: load static data i.e tile-set or shape-file data 
+  * Go to https://studio.mapbox.com/tilesets/
+  * Click on "New tileSet" button and upload an tile set (shape-file) "refer screenshot src/screenshot/tile-set.png"
+  * 
+  * once map is loaded add a "source" to the map, A "source" is a shape-file which can have multiple layers
+  * If want to do some interactions over map then add those events to "source" (not on layer). A layer is a part of source.
+  */
 
 function initMap(lat, lng, zoom) {
   window.mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
@@ -14,6 +22,35 @@ function initMap(lat, lng, zoom) {
   return map
 }
 
+function addSource_fromShapeFile(map) {
+  map.on('load', () => {
+    map.addSource('anyNameButSameAsSource', { // other wise layer will not be visible
+    type: 'vector',
+    url: 'mapbox://asaman.0wyn6aet', // map id from mapbox
+    "minzoom": 0,
+    "maxzoom": 14
+    });
+
+    map.addLayer(
+    {
+    'id': 'highway-data', // any name
+    'type': 'line',
+    'source': 'anyNameButSameAsSource', //  map.addSource('anyNameButSameAsSource'... is referring here
+    'source-layer': 'delhi_highway-329dkv', // uploaded shape-file name
+    'layout': {
+    'line-join': 'round',
+    'line-cap': 'round'
+    },
+    'paint': {
+    'line-color': '#F5EA5A',
+    'line-width': 3
+    }
+    },
+    );
+    });
+}
+
+
 function App() {
   const [lat, setLat] =  useState(77.08496980000001)
   const [lng, setLng] =  useState(28.644934149999997)
@@ -21,6 +58,7 @@ function App() {
 
   useEffect(() => {
     const mapObj = initMap(lat, lng, zoom)
+    addSource_fromShapeFile(mapObj)
   },[])
 
 
