83d928938535b12818a95aee12e45131eff23b34
amandhunna <amandhunna.dev@gmail.com>
2023-03-14 16:44:20 +0530

    feat: added source from geojson

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import React, {useState, useEffect} from 'react'
   * 
   * once map is loaded add a "source" to the map, A "source" is a shape-file which can have multiple layers
   * If want to do some interactions over map then add those events to "source" (not on layer). A layer is a part of source.
+  * 
+  * STEP 2.2: load dynamic data i.e geojson 
+  * Go to https://geojson.io/ and create some route using line geometry and copy that JSON to addSource_fromGeojson function
+  * similar to step 2.1 now we need to add layer to show data over map
   */
 
 function initMap(lat, lng, zoom) {
@@ -50,6 +54,61 @@ function addSource_fromShapeFile(map) {
     });
 }
 
+function addSource_fromGeojson(map) {
+  // https://geojson.io/
+  map.on('load', () => {
+    map.addSource('routeFromGeoJson', {
+    'type': 'geojson',
+    'data': {
+      'type': 'Feature',
+      'properties': {},
+      'geometry': {
+      'type': 'LineString',
+      'coordinates': [
+          [
+            77.13036481534817,
+            28.636150039381945
+          ],
+          [
+            77.13706001149444,
+            28.61781455366885
+          ],
+          [
+            77.13706001149444,
+            28.612407324720337
+          ],
+          [
+            77.14268397625665,
+            28.606999817388683
+          ],
+          [
+            77.1560743685493,
+            28.60323790898849
+          ],
+          [
+            77.23186398892358,
+            28.628628176280145
+          ]
+        ],
+      }
+    }
+  });
+
+    map.addLayer({
+      'id': 'route',
+      'type': 'line',
+      'source': 'routeFromGeoJson',
+      'layout': {
+        'line-join': 'round',
+        'line-cap': 'round'
+      },
+    'paint': {
+        'line-color': '#888',
+        'line-width': 8
+      }
+    });
+  })
+}
 
 function App() {
   const [lat, setLat] =  useState(77.08496980000001)
@@ -59,6 +118,7 @@ function App() {
   useEffect(() => {
     const mapObj = initMap(lat, lng, zoom)
     addSource_fromShapeFile(mapObj)
+    addSource_fromGeojson(mapObj)
   },[])
 
 
