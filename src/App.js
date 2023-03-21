c15be4d28a320a8735cd97764e55c5b8aaf3af5b
amandhunna <amandhunna.dev@gmail.com>
2023-03-21 17:58:50 +0530

    chore

--- src/App.js
+++ src/App.js
@@ -56,23 +56,7 @@ function addSource_fromShapeFile(map) {
     },
     'paint': {
       'line-width': 3,
-      'line-color':   [
-          'case',
-
-            ['in', ['get', 'TYPE'],'primary'],
-            '#F2921D',
-            ['in', ['get', 'TYPE'],'secondary'],
-            '#FFD966',
-            ['in', ['get', 'TYPE'],'tertiary'],
-            '#F4B183',
-            ['==', ['get', 'TYPE'],'residential'],
-            '#FFF2CC',
-            ['in', ['get', 'TYPE'],'trunk'],
-            '#DFA67B',
-            ['in', ['get', 'TYPE'],'construction'],
-            'red',
-            'black'
-            ]
+      'line-color':  'black'
       }
     });
   });
